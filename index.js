f1f8967066939912766dfaf905bdde15df3c0816
watcharintojuang <85301555+watcharintojuang@users.noreply.github.com>
2022-01-23 08:58:16 +0700

    Summary
    -change folder templates to public (manage static files)

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+const exp = require('constants')
 const express = require('express')
-const router = require('./routes/myRouter')
-
+const path = require('path')
 const app = express()
 
-
-app.use(router)
+app.use(express.static(path.join(__dirname,'public')))
 
 app.listen(8080,()=>{
     console.log('start port 8080')
