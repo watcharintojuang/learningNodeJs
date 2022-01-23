ef1c9a8b9921f44c2bfecad37dd47eef4f4ad30d
watcharintojuang <85301555+watcharintojuang@users.noreply.github.com>
2022-01-23 13:33:36 +0700

    Summary

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const router = require("./routes/myRouter");
+const app = express();
+
+//จัดการviews template dinamic
+app.set("views", path.join(__dirname, "views")); //อ้างอิงตำแหน่ง folder views
+app.set("view engine", "ejs"); //เราใช้template ตัวไหนต้องตั้งค่าก่อน นามสกุลของไฟล์ด้วย
+app.use(express.urlencoded({extended:false})) //ต้งค่าสำหรับการรับข้อมูลแบบpost method
+
+app.use(router);
+
+//จัดการstatic file
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(8080, () => {
+  console.log("start port 8080");
+});
--- routes/myRouter.js
+++ routes/myRouter.js
@@ -1,5 +1,27 @@
-const express = require('express')
-const router = express.Router()
-const path = require('path')
+const express = require("express");
+const router = express.Router();
 
-module.exports = router
+
+router.get("/", (req, res) => {
+  const products = [
+      {name:'Computer',price:25000,image:'/images/products/product1.png'},
+      {name:'เสื้อผ้า',price:299,image:'/images/products/product2.png'},
+      {name:'หูฟัง',price:4000,image:'/images/products/product3.png'}
+  ]
+  res.render("index", {products: products});
+});
+
+router.get("/addForm",(req,res)=>{
+    res.render('form')
+})
+
+router.get("/manage",(req,res)=>{
+    res.render('manage')
+})
+
+router.post('/insert',(req,res)=>{
+    console.log(req.body)
+    res.render('form')
+})
+
+module.exports = router;
