b1c36ed1ef8e27c03c3ede89b58048567a8ff943
watcharintojuang <85301555+watcharintojuang@users.noreply.github.com>
2022-01-26 19:36:54 +0700

    summary

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 //จัดการviews template dinamic
 app.set("views", path.join(__dirname, "views")); //อ้างอิงตำแหน่ง folder views
 app.set("view engine", "ejs"); //เราใช้template ตัวไหนต้องตั้งค่าก่อน นามสกุลของไฟล์ด้วย
+
 app.use(express.urlencoded({extended:false})) //ต้งค่าสำหรับการรับข้อมูลแบบpost method
 
 app.use(router);
--- routes/myRouter.js
+++ routes/myRouter.js
@@ -27,14 +27,22 @@ router.get("/", (req, res) => {
   
 });
 
-router.get("/addForm",(req,res)=>{
+router.get("/add-product",(req,res)=>{
     res.render('form')
 })
+
 
 router.get("/manage",(req,res)=>{
     Product.find().exec((err,doc)=>{
         res.render("manage", {products: doc}); 
       })
+})
+
+router.get("/delete/:id",(req,res)=>{
+    Product.findByIdAndDelete(req.params.id,{useFindAndModify:false}).exec(err=>{
+        if(err) console.log(err)
+        res.redirect('/manage')
+    })
 })
 
 router.post('/insert',upload.single("image"),(req,res)=>{
@@ -48,6 +56,35 @@ router.post('/insert',upload.single("image"),(req,res)=>{
         if(err) console.log(err)
     })
     res.redirect('/')
+})
+
+
+router.get("/:id",(req,res)=>{
+    const product_id = req.params.id
+    Product.findOne({_id:product_id}).exec((err,doc)=>{
+        res.render('product',{product:doc})
+    })
+})
+
+router.post('/edit',(req,res)=>{
+    const edit_id = req.body.edit_id
+    Product.findOne({_id:edit_id}).exec((err,doc)=>{
+        //นำข้อมูลเดิมที่ต้องการแก้ไข ไปแสดงในแบบฟอร์ม
+        res.render('edit',{product:doc})
+    })
+})
+
+router.post('/update',(req,res)=>{
+    //ข้อมูลใหม่ที่ถูกส่งมาจากฟอร์มแก้ไข
+    const update_id = req.body.update_id
+    let data = ({
+        name:req.body.name,
+        price:req.body.price,
+        description:req.body.description
+    })
+    Product.findByIdAndUpdate(update_id,data,{useFindAndModify:false}).exec(err=>{
+        res.redirect("/manage")
+    })
 })
 
 module.exports = router;
